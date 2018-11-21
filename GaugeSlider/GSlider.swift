63043ba4d852a2d8f162b2ae219b480230ce66d2
Phil Townsend <>
2018-11-21 15:22:24 -0500

    BR1 - initial commit

---
+++ GaugeSlider/GSlider.swift
@@ -0,0 +1,13 @@
+//
+//  Slider.swift
+//  GaugeSlider
+//
+//  Created by Phil Townsend on 11/21/18.
+//  Copyright © 2018 Phil Townsend. All rights reserved.
+//
+
+import UIKit
+
+class GSlider: UIControl {
+
+}
---
+++ GaugeSlider/SliderPathLayer.swift
@@ -0,0 +1,13 @@
+//
+//  SliderPathLayer.swift
+//  GaugeSlider
+//
+//  Created by Phil Townsend on 11/21/18.
+//  Copyright © 2018 Phil Townsend. All rights reserved.
+//
+
+import UIKit
+
+class SliderPathLayer: CALayer {
+
+}
